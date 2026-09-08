b6ee44b8426d33bff47e2cdc2387955a817a57a2
Pedro Widya <edrzvena@gmail.com>
2026-09-08 19:41:30 +0700

    chore: add LoginViewModel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+// ViewModels/LoginViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace NetMvcAuth.ViewModels;
+
+public class LoginViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    public bool RememberMe { get; set; }
+}
